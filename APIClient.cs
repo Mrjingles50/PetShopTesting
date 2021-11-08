cb8a0cba5c4f06285a6d6e81ba1047658ef1d21e
Mrjingles50 <60758072+Mrjingles50@users.noreply.github.com>
2021-11-08 14:06:38 -0500

    Added APIClient class for testing, Added tests for all current methods in MonitoringAPIClient.cs

---
+++ APIClient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using PetShopMetrics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopTesting
+{
+    class APIClient : IDisposable
+    {
+        public MonitoringAPIClient _api { get; private set; }
+        private HttpClient client;
+        Uri path = new Uri("https://api20211105132600.azurewebsites.net/api/");
+
+        public APIClient()
+        {
+            client = new();
+            var webBuilder = new WebHostBuilder();
+            webBuilder.UseStartup<Startup>();
+            client.BaseAddress = path;
+            _api = new MonitoringAPIClient(client);
+        }
+
+        public void Dispose()
+        {
+            client?.Dispose();
+        }
+    }
+}
--- MonitoringAPIClientTests.cs
+++ MonitoringAPIClientTests.cs
@@ -1,33 +1,79 @@
-using Microsoft.AspNetCore.Hosting;
-using PetShopMetrics;
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 using Xunit;
+using FluentAssertions;
+using PetShopMetrics.Models;
 using System;
 
 namespace PetShopTesting 
 {
     public class MonitoringAPIClientTests 
-    {
-        public static MonitoringAPIClient _api;
-        public static HttpClient client = new HttpClient();
-        Uri path = new Uri("https://api20211105132600.azurewebsites.net/api/");
-        public MonitoringAPIClientTests()
-        {
-            var webBuilder = new WebHostBuilder();
-            webBuilder.UseStartup<Startup>();
-            client.BaseAddress = path;
-            _api = new MonitoringAPIClient(client);
-        }
-
+    {     
         [Fact]
         public async Task TestGetDistinctCategories()
         {
-            IEnumerable<string> result = await _api.GetDistinctCategories();
-            Assert.Equal(7, result.Count());
+            using var api = new APIClient();
+            IEnumerable<string> result = await api._api.GetDistinctCategories();
+            
+            //In range of 8 to 9 due to "Test" category being added and removed
+            result.Count().Should().BeInRange(8, 9);
         }
+
+        [Fact]
+        public async Task TestGetMerchandiseFilter()
+        {
+            using var api = new APIClient();
+            IEnumerable<MerchandiseFilter> result = await api._api.GetMerchandiseFilter();
+            
+            result.Count().Should().BeInRange(0, 500);
+        }
+
+        [Fact]
+        public async Task TestGetMerchandisByCategory()
+        {
+            using var api = new APIClient();
+            IEnumerable<MerchandiseFilter> result = await api._api.GetMerchandiseByCategory("Dog");
+            
+            result.First().Category.Should().Be("Dog");
+            result.Last().Category.Should().Be("Dog");
+        }
+
+        [Fact]
+        public async Task TestGetMerchandisByMonth()
+        {
+            using var api = new APIClient();
+            IEnumerable<MerchandiseFilter> result = await api._api.GetMerchandiseByMonth(10);
+            
+            result.First().DateAndTime.Month.Should().Be(10);
+            result.Last().DateAndTime.Month.Should().Be(10);
+        }
+
+        [Fact]
+        public async Task TestGetMerchandiseSearchCountByMonthAndCategory()
+        {
+            using var api = new APIClient();
+            int result = await api._api.GetMerchandiseSearchCountByMonthAndCategory(11, "Test");
+
+            result.Should().BeInRange(0, 4);
+
+            result = await api._api.GetMerchandiseSearchCountByMonthAndCategory(10, "Dog");
+
+            result.Should().NotBeInRange(0, 4);
+        }
+
+        [Fact]
+        public async Task TestGetMerchandiseByValueAndTimeSpan()
+        {
+            using var api = new APIClient();
+            IEnumerable<MerchandiseFilter> result = await api._api.GetMerchandiseByValueAndTimeSpan("Test",  new DateTime(2021,11,1) ,DateTime.Now );
+
+            result.First().DateAndTime.Month.Should().Be(11);
+            result.Last().DateAndTime.Month.Should().Be(11);
+            result.Count().Should().Be(4);
+        }
+        
+
     }
 }
